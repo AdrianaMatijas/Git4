2236e448c1760cbe7dd3f391a337bc133b0f2c43
AdrianaMatijas <adriana.matijas@viahub.eu>
2017-02-24 10:36:00 +0100

    adeed 'e' to test

--- ProjectGit/src/ClassGit2.java
+++ ProjectGit/src/ClassGit2.java
@@ -4,7 +4,7 @@ public class ClassGit2 {
 	public static void main(String[] args) {
 		System.out.println("test git 2");
 		System.out.println("test git 3");
-		System.out.println("test gitt changees");
+		System.out.println("teest gitt changees");
 	}
 
 }
